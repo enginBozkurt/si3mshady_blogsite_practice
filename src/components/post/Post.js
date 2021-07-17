fa97b596da6e0b3774faa735465dc323adb2af2d
Elliott Arnold <si3mshady@Elliotts-MBP.attlocal.net>
2021-07-17 08:22:04 -0500

    added dev branch

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import './post.css'
 
 export default function Post() {
     return (
         <div className="post">
-            Single Post 
+           <img 
+           className="postImage" alt=""
+           src="https://upload.wikimedia.org/wikipedia/en/d/d4/Nightcrawlerfilm.jpg"></img> 
+
+           <div className="postInfo">
+               <div className="postCategories">                     
+                    <span className="postCat">Code</span>
+                    <span className="postCat">Movies</span>
+                    <span className="postCat">Sports</span>
+               </div>
+               <span className="postTitle">
+                   lorem ipsum dolor sit amet
+               </span>
+               <span className="postDate">
+                 1 hour ago.
+               </span>
+           </div>
+
         </div>
     )
 }
