51fe5f2a0aee19f3f0f66445acbbacaf295286fc
Elliott Arnold <si3mshady@Elliotts-MBP.attlocal.net>
2021-07-17 09:19:39 -0500

    save point

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import './post.css'
 
-export default function Post() {
+export default function Post({image}) {
+    // const imgURL = image !== null ? image : url
+
+    const imgURL = "https://res.cloudinary.com/culturemap-com/image/upload/ar_4:3,c_fill,g_faces:center,w_980/v1508806983/photos/263174_original.jpg"
     return (
         <div className="post">
+               
            <img 
            className="postImage" alt=""
-           src="https://upload.wikimedia.org/wikipedia/en/d/d4/Nightcrawlerfilm.jpg"></img> 
+      
+           src={imgURL}></img> 
 
            <div className="postInfo">
                <div className="postCategories">                     
@@ -15,11 +20,16 @@ export default function Post() {
                     <span className="postCat">Sports</span>
                </div>
                <span className="postTitle">
-                   lorem ipsum dolor sit amet
+                   lorem ipsum dolor  lorem ipsum dolor
                </span>
                <span className="postDate">
                  1 hour ago.
                </span>
+               <p className="postDescription">lorem lorem ipsum dolor ipsumlorem lorem 
+               ipsum dolor ipsum dds kirb dds llorem lorem ipsum dolor ipsum dds kirblorem lorem ipsum dolor ipsum dds kirblorem lorem ipsum dolor ipsum dds kirblorem lorem ipsum dolor ipsum 
+               dds kirblorem lorem ipsum dolor ipsum dds lorem lorem ipsum dolor ipsumlorem lorem 
+               ipsum dolor ipsum dds kirb dds llorem lorem ipsum dolor ipsum dds kirblorem lorem ipsum dolor ipsum dds kirblorem lorem ipsum dolor ipsum dds kirblorem lorem ipsum dolor ipsum 
+               dds kirblorem lorem ipsum dolor ipsum dds kirborem lorem ipsum kirborem lorem ipsum dolor ipsum dds kirblorem lorem ipsum dolor ipsum dds kirbkirb </p>
            </div>
 
         </div>
